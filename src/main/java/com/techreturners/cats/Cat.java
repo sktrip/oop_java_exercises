f882cb6b71bfdbf01c506deada5be0ecf8bd4af7
sktrip <sktripurari@gmail.com>
2020-12-02 20:53:41 +0000

    Cat interfacec

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -1,5 +1,11 @@
 package com.techreturners.cats;
 
-interface Cat {
 
+public interface Cat {
+    public boolean isAsleep();
+    public void goToSleep();
+    public void wakeUp();
+    public String getSetting();
+    public int getAverageHeight();
+    public String eat();
 }
